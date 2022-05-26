8cb0ca36a0ed29c49bbdccf5afdd6dcc400d2a0f
midi_cable <bogdan.astronomy@gmail.com>
2022-05-26 17:03:38 +0300

    (lab2) secant method done

--- (lab2)-Nonliner_System/main.py
+++ (lab2)-Nonliner_System/main.py
@@ -1,8 +1,26 @@
-from methods import SecantMethod, GaussSeidelMethod
+from methods import*
 import numpy as np
-x_vector1 = GaussSeidelMethod(2, 2)
-x_vector2 = SecantMethod(2, 0.5, 10**(-7))
 
+X_Gauss_Seidel, Y_Gauss_Seidel, Iterations_Gauss_Seidel = GaussSeidelMethod(3, 0.5, 10**(-7))
+X_Secant_Method, Y_Secant_Method, Iterations_Secant_Method = SecantMethod(3, 0.5, 10**(-7))
+
+print('Gauss Seidel Results:')
+print(f'(x, y) = ({X_Gauss_Seidel}, {Y_Gauss_Seidel})')
+print('f1(x*, y*) =', func1(X_Gauss_Seidel, Y_Gauss_Seidel), sep=' ')
+print('f2(x*, y*) =', func2(X_Gauss_Seidel, Y_Gauss_Seidel), sep=' ')
+print('||(f1(x*, y*), f2(x*, y*))|| =', np.linalg.norm([func1(X_Gauss_Seidel, Y_Gauss_Seidel),
+                                                         func2(X_Gauss_Seidel, Y_Gauss_Seidel)], np.inf))
+print('iterations =', Iterations_Gauss_Seidel)
+
+print()
+
+print('Secant Method Results:')
+print(f'(x, y) = ({X_Secant_Method}, {Y_Secant_Method})')
+print('f1(x*, y*) =', func1(X_Secant_Method, Y_Secant_Method), sep=' ')
+print('f2(x*, y*) =', func2(X_Secant_Method, Y_Secant_Method), sep=' ')
+print('||(f1(x*, y*), f2(x*, y*))|| =', np.linalg.norm([func1(X_Secant_Method, Y_Secant_Method),
+                                                         func2(X_Secant_Method, Y_Secant_Method)], np.inf))
+print('iterations =', Iterations_Secant_Method)
 
 
 
--- (lab2)-Nonliner_System/methods.py
+++ (lab2)-Nonliner_System/methods.py
@@ -9,8 +9,16 @@ def func2(x, y):
     return 2 * y - sin(x - 1)
 def func2FirstDerivative(x, y):
     return 2
-def GaussSeidelMethod(x0, y0):
-    eps = 10**(-7)
+def func1XPartialDerivative(x, y):
+    return 2 * (x - 1)
+def func1YPartialDerivative(x, y):
+    return 2 * y
+def func2XPartialDerivative(x, y):
+    return -0.5 * cos(x - 1)
+def func2YPartialDerivative(x, y):
+    return 1
+
+def GaussSeidelMethod(x0, y0, eps):
     iterations = 1
 
     x_prev_GaussSeidel, y_prev_GaussSeidel = x0, y0
@@ -59,38 +67,32 @@ def GaussSeidelMethod(x0, y0):
                      func2FirstDerivative(x_curr_GaussSeidel, y_prev_Newton)
         y_curr_GaussSeidel = y_curr_Newton
 
-    print(x_curr_GaussSeidel, y_curr_GaussSeidel)
-    print(func1(x_curr_GaussSeidel, y_curr_GaussSeidel))
-    print(func2(x_curr_GaussSeidel, y_curr_GaussSeidel))
+    return x_curr_GaussSeidel, y_curr_GaussSeidel, iterations
 #---------------------------------------
 
-def func1XPartialDerivative(x, y):
-    return 2 * (x - 1)
-def func1YPartialDerivative(x, y):
-    return 2 * y
-def func2XPartialDerivative(x, y):
-    return -0.5 * cos(x - 1)
-def func2YPartialDerivative(x, y):
-    return 1
+
 def SecantMethod(x0, y0, eps):
     v0 = np.array([[x0], [y0]])
-    J = np.array([[func1XPartialDerivative(x0, y0), func1YPartialDerivative(x0, y0)],
-                  [func2XPartialDerivative(x0, y0), func2YPartialDerivative(x0, y0)]])
+    J = [[func1XPartialDerivative(x0, y0), func1YPartialDerivative(x0, y0)],
+         [func2XPartialDerivative(x0, y0), func2YPartialDerivative(x0, y0)]]
 
-    delta = np.linalg.inv(J).dot(np.array([[(-1) * func1(v0[0][0], v0[1][0])],
-                                           [(-1) * func2(v0[0][0], v0[1][0])]]))
+    delta = np.linalg.solve(J, [[(-1) * func1(v0[0][0], v0[1][0])], [(-1) * func2(v0[0][0], v0[1][0])]])
     v1 = v0 + delta
+
+    iterations = 1
     while np.linalg.norm(v1 - v0, ord=np.inf) > eps:
-        J = np.array([[(func1(v1[0][0], v1[1][0]) - func1(v0[0][0], v1[1][0])) / (v1[0][0] - v0[0][0]),
-                       (func1(v1[0][0], v1[1][0]) - func1(v1[0][0], v0[1][0])) / (v1[1][0] - v0[1][0])],
-                      [(func2(v1[0][0], v1[1][0]) - func2(v0[0][0], v1[1][0])) / (v1[0][0] - v0[0][0]),
-                       (func2(v1[0][0], v1[1][0]) - func2(v1[0][0], v0[1][0])) / (v1[1][0] - v0[1][0])]])
+        J = [[(func1(v1[0][0], v1[1][0]) - func1(v0[0][0], v1[1][0])) / (v1[0][0] - v0[0][0]),
+              (func1(v1[0][0], v1[1][0]) - func1(v1[0][0], v0[1][0])) / (v1[1][0] - v0[1][0])],
+             [(func2(v1[0][0], v1[1][0]) - func2(v0[0][0], v1[1][0])) / (v1[0][0] - v0[0][0]),
+              (func2(v1[0][0], v1[1][0]) - func2(v1[0][0], v0[1][0])) / (v1[1][0] - v0[1][0])]]
 
-        v2 = v1 - np.linalg.inv(J).dot(np.array([[(-1) * func1(v0[0][0], v0[1][0])],
-                                                 [(-1) * func2(v0[0][0], v0[1][0])]]))
-
-
+        delta = np.linalg.solve(J, [[(-1) * func1(v1[0][0], v1[1][0])], [(-1) * func2(v1[0][0], v1[1][0])]])
+        v2 = v1 + delta
         v0 = v1
         v1 = v2
+        iterations += 1
 
-    return v1
+    x_curr_secant = v1[0][0]
+    y_curr_secant = v1[1][0]
+
+    return x_curr_secant, y_curr_secant, iterations
